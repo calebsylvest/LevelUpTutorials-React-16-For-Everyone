df577d7d6c27dc36a65be83c393ec6cbf7a8c5bb
Caleb Sylvest <caleb.sylvest@gmail.com>
2020-03-29 17:04:42 -0500

    Complete 9. Iterating and Child Components

--- src/App.js
+++ src/App.js
@@ -2,48 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
 
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spiderman'
+  },
+  {
+    id: 3,
+    title: 'No Country For Old Men'
+  },
+  {
+    id: 4,
+    title: 'Inception'
+  },
+];
 
 class App extends Component {
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    console.log(event.target.value)
-    this.setState({
-      input: event.target.value.trim()
-    })
-  }
-
-  submit = () => {
-    console.log(this.text.value)
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to React Props" />
-          <h3>{this.state.input}</h3>
-          <input type="text" onChange={this.updateInput} value={this.state.input} />
-          <input type="text" ref={(input) => this.text = input } />
-          <button onClick={this.submit}>Show Value</button>
-          
         </header>
+        {movies.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    
-    return (
-      <h1 className="App-title">{text}</h1>
-    )
   }
 }
 
