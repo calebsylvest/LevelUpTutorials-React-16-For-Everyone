01afae0b6714072ed44687a4046321f7f8266ced
Caleb Sylvest <caleb.sylvest@gmail.com>
2020-03-29 12:54:00 -0500

    Complete 8. Inputs. Controlled vs Uncontrolled.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import './App.css';
 
 
 class App extends Component {
+  state = {
+    input: 'Hello'
+  }
+
+  updateInput = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      input: event.target.value.trim()
+    })
+  }
 
   submit = () => {
     console.log(this.text.value)
@@ -16,7 +26,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Welcome text="Welcome to React Props" />
-          
+          <h3>{this.state.input}</h3>
+          <input type="text" onChange={this.updateInput} value={this.state.input} />
           <input type="text" ref={(input) => this.text = input } />
           <button onClick={this.submit}>Show Value</button>
           
